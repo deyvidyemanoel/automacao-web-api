37787425e4a33442c5e653c61017f67402d2b279
deyvidyemanoel <deyvidyemanoel21@gmail.com>
2026-05-07 13:47:06 -0300

    fix: aguarda cart container carregar antes de contar itens

--- web/pages/cart_page.py
+++ web/pages/cart_page.py
@@ -2,16 +2,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from web.pages.base_page import BasePage
 
-
 class CartPage(BasePage):
     CART_ITEMS = (By.CLASS_NAME, "cart_item")
     CHECKOUT_BUTTON = (By.ID, "checkout")
+    CART_CONTAINER = (By.CLASS_NAME, "cart_contents_container")
 
     def get_item_count(self):
-        try:
-            self.wait.until(EC.presence_of_element_located(self.CART_ITEMS))
-        except Exception:
-            pass
+        self.wait.until(EC.presence_of_element_located(self.CART_CONTAINER))
         return len(self.driver.find_elements(*self.CART_ITEMS))
 
     def proceed_to_checkout(self):
